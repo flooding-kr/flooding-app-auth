8d13172dc2938b54e1ca337f2954ee50ab75e07c
parkyuhyeon <pyh1111802@naver.com>
2025-02-12 17:08:03 +0900

    chore :: 경로 수정

---
+++ lib/feature/splash/presentation/splash_presentation.dart
@@ -0,0 +1,7 @@
+import 'package:flooding_app_design_system/flooding_app_design_system.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part './views/splash_page.dart';
+part './widgets/splash_scaffold.dart';
+part './widgets/splash_logo.dart';
