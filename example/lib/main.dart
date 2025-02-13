215c38ee8c3afef5dea188629102c56e363011ba
parkyuhyeon <pyh1111802@naver.com>
2025-02-13 20:49:13 +0900

    fix ::  example 파일 수정

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flooding_app_auth/feature/splash/presentation/splash_presentation.dart';
+import 'package:flooding_app_auth/router.dart';
 import 'package:flooding_app_design_system/flooding_app_design_system.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -11,11 +13,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(
         scaffoldBackgroundColor: FloodingColor.gray100,
       ),
-      home: SplashPage(),
+      routerConfig: AuthRouter.router
     );
   }
 }
