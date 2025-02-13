bcbfcdc83a581a6de36da163462ab5d5fd590914
parkyuhyeon <pyh1111802@naver.com>
2025-02-14 08:21:29 +0900

    fix :: print 오류 해결

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flooding_app_auth/feature/splash/presentation/splash_presentation.dart';
 import 'package:flooding_app_auth/router.dart';
 import 'package:flooding_app_design_system/flooding_app_design_system.dart';
 import 'package:flutter/material.dart';
--- lib/feature/splash/presentation/bloc/splash_bloc.dart
+++ lib/feature/splash/presentation/bloc/splash_bloc.dart
@@ -12,9 +12,7 @@ class SplashBloc extends Bloc<SplashEvent, SplashState> {
   void _handlerSplashToSignEvent(SplashEvent event, Emitter<SplashState> emit) async{
     emit(SplashLoadingState());
     // shared preference에서 캐싱한 로그인 정보 (id, pw or token)를 불러와 로그인을 합니다
-    print('start');
     await Future.delayed(Duration(seconds: 3));
-    print('stop');
     // shared preference에서 캐싱한 로그인 정보 (id, pw or token)를 불러와 로그인을 합니다
     emit(SplashDisposeState());
   }
