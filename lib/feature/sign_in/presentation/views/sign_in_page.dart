1566c1775014718d43071af34897f1b7610ed418
parkyuhyeon <pyh1111802@naver.com>
2025-02-14 10:48:29 +0900

    linting design :: sign in page 퍼블리싱

--- lib/feature/sign_in/presentation/views/sign_in_page.dart
+++ lib/feature/sign_in/presentation/views/sign_in_page.dart
@@ -1,10 +1,27 @@
 part of '../sign_in_presentation.dart';
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({super.key});
+  SignInPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return _Scaffold(
+      logo: _Logo(),
+      emailTextfield: _EmailTextField(controller: emailController),
+      passwordTextfield: _PasswordTextField(controller: passwordController),
+      loginButton: _LoginButton(
+        onTap: () {},
+        color: FloodingColor.gray500,
+      ),
+      signUpTextButton: _SignUpTextButton(
+        onTap: () {},
+      ),
+      findPasswordButton: _FindPasswordButton(
+        onTap: () {},
+      ),
+    );
   }
 }
