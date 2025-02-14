066a7388a1fcb51fefd89c7e7de63bddd809b7bb
parkyuhyeon <pyh1111802@naver.com>
2025-02-14 10:48:16 +0900

    linting design :: sign in email textfield 퍼블리싱

---
+++ lib/feature/sign_in/presentation/widgets/sign_in_email_textfield.dart
@@ -0,0 +1,15 @@
+part of '../sign_in_presentation.dart';
+
+class _EmailTextField extends StatelessWidget {
+  const _EmailTextField({required this.controller});
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloodingTextField(
+        textFieldState: FloodingTextFieldState.email,
+        controller: controller,
+        hintText: '이메일을 입력해주세요');
+  }
+}
