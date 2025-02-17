4f997203ede1f0cf6a7b6e9209b520e3e4c46870
answad <mymhj122@gmail.com>
2025-02-17 17:55:15 +0900

    chore :: revert commit

--- lib/feature/sign_up/presentation/views/sign_up_page.dart
+++ lib/feature/sign_up/presentation/views/sign_up_page.dart
@@ -5,6 +5,11 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const _NameScaffold();
+    return Scaffold(
+        body: SignUpCard(
+      title: '과나',
+      content: Container(),
+      onBackClick: () {},
+    ));
   }
 }
