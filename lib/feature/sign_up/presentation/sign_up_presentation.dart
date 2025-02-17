9cecea0b00491a4c526f6e83f8ca405250db4197
answad <mymhj122@gmail.com>
2025-02-17 17:19:40 +0900

    linting design :: SignUpCard 퍼블리싱

--- lib/feature/sign_up/presentation/sign_up_presentation.dart
+++ lib/feature/sign_up/presentation/sign_up_presentation.dart
@@ -1,5 +1,6 @@
+import 'package:flooding_app_design_system/flooding_app_design_system.dart';
 import 'package:flutter/material.dart';
 
 part 'views/sign_up_page.dart';
-
+part 'widgets/sign_in_card.dart';
 part 'widgets/name/sign_up_name_scaffold.dart';
