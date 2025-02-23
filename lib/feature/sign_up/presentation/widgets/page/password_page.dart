623415674da1c24dedc694d3afd015cf1b6f4161
parkyuhyeon <pyh1111802@naver.com>
2025-02-23 19:50:54 +0900

    linting design :: password 페이지 퍼블리싱

--- lib/feature/sign_up/presentation/widgets/page/password_page.dart
+++ lib/feature/sign_up/presentation/widgets/page/password_page.dart
@@ -1,7 +1,6 @@
 part of '../../sign_up_presentation.dart';
 
 class _PasswordPage extends StatelessWidget {
-
   final TextEditingController passwordController;
   final VoidCallback onBackClick;
   final VoidCallback onNextClick;
@@ -14,20 +13,35 @@ class _PasswordPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(16, 36, 16, 79),
-          child: Column(
-            children: [
-              SignUpCard(
-                title: '비밀번호를 입력해주세요',
-                content: Container(),
-                onBackClick: onBackClick,
+    return BlocBuilder<PasswordBloc, PasswordState>(
+      builder: (context, state) => Padding(
+        padding: EdgeInsets.fromLTRB(16, 36, 16, 79),
+        child: Column(
+          children: [
+            SignUpCard(
+              title: '비밀번호를 입력해주세요',
+              content: FloodingTextField(
+                textStyle: FloodingTypography.body3Regular.copyWith(letterSpacing: 0),
+                textFieldState: FloodingTextFieldState.password,
+                controller: context.read<PasswordBloc>().controller,
+                hintText: '비밀번호를 입력해주세요',
+                validator: context.read<PasswordBloc>().validatePassword,
               ),
-              SizedBox(height: double.infinity),
-              FloodingDefaultButton(onTap: onNextClick, text: "다음"),
-            ],
-          ),
-        ));
+              onBackClick: onBackClick,
+            ),
+            Spacer(),
+            FloodingDefaultButton(
+              onTap: state is EnablePasswordState ? onNextClick : () {},
+              text: "다음",
+              textStyle: FloodingTypography.body3SemiBold,
+              padding: EdgeInsets.symmetric(vertical: 12.h),
+              color: state is EnablePasswordState
+                  ? FloodingColor.main600
+                  : FloodingColor.gray400,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
