95d11ec024224a32eb49acc4ede78a3b52b18a5c
parkyuhyeon <pyh1111802@naver.com>
2025-02-23 19:50:38 +0900

    feat :: import 추가

--- lib/feature/sign_up/presentation/sign_up_presentation.dart
+++ lib/feature/sign_up/presentation/sign_up_presentation.dart
@@ -1,7 +1,9 @@
+import 'package:flooding_app_auth/feature/sign_in/presentation/sign_in_presentation.dart';
 import 'package:flooding_app_design_system/flooding_app_design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 part 'widgets/sign_in_card.dart';
 
@@ -25,6 +27,18 @@ part 'bloc/class/class_event.dart';
 
 part 'bloc/class/class_state.dart';
 
+part 'bloc/password/password_bloc.dart';
+
+part 'bloc/password/password_state.dart';
+
+part 'bloc/password/password_event.dart';
+
+part 'bloc/sex/sex_bloc.dart';
+
+part 'bloc/sex/sex_state.dart';
+
+part 'bloc/sex/sex_event.dart';
+
 part 'widgets/page/name_page.dart';
 
 part 'widgets/page/email_page.dart';
@@ -34,3 +48,7 @@ part 'widgets/page/password_page.dart';
 part 'widgets/page/class_page.dart';
 
 part 'widgets/page/sex_page.dart';
+
+part 'widgets/page/certification_number_page.dart';
+
+
